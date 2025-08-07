f6271d2b285152677ab16fab4b393f7ce6336802
SanjayVJ22 <147210384+SanjayVJ22@users.noreply.github.com>
2025-08-07 20:57:14 +0100

    Contact section

---
+++ src/sections/Contact/Contact.jsx
@@ -0,0 +1,34 @@
+import styles from './ContactStyles.module.css'
+
+function Contact() {
+  return (
+    <section id='contact' className={styles.container}>
+        <h1 className='sectionTitle'>Contact
+            <form action="">
+                <div className='formGroup'>
+                    <label htmlFor='name' hidden>
+                        Name
+                    </label>
+                    <input type='text' name='name' id='name' placeholder='Name' required/>
+                </div>
+                <div className='formGroup'>
+                    <label htmlFor='email' hidden>
+                        email
+                    </label>
+                    <input type='text' email='email' id='email' placeholder='Email' required/>
+                </div>
+                <div className='formGroup'>
+                    <label htmlFor='message' hidden>
+                        Name
+                    </label>
+                    <textarea name='message' id='message' placeholder='Message' required>
+                    </textarea>
+                </div>
+                <input className='hover btn' type='submit' value='Submit'></input>
+            </form>
+        </h1>
+    </section>
+  )
+}
+
+export default Contact
