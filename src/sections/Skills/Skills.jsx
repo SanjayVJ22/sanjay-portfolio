e0abb6978f9915f698c4b866d8f587d0438f5b1b
SanjayVJ22 <147210384+SanjayVJ22@users.noreply.github.com>
2025-08-07 20:30:45 +0100

    Skills commit

---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,28 @@
+import styles from './SkillsStyles.module.css'
+import checkMarkIcon from '../../assets/checkmark-dark.svg'
+import SkillList from '../../common/SkillList'
+function Skills() {
+  return (
+    <section id='skills' className={styles.container}>
+        <h1 className='sectionTitle'>Skills</h1>
+        <div className={styles.skillList}>
+            <SkillList src={checkMarkIcon} skill='Java' />
+            <SkillList src={checkMarkIcon} skill='HTML' />
+            <SkillList src={checkMarkIcon} skill='CSS' />
+            <SkillList src={checkMarkIcon} skill='JavaScript' />
+            <SkillList src={checkMarkIcon} skill='Node' />
+        </div>
+        <hr />
+           <div className={styles.skillList}>
+            <SkillList src={checkMarkIcon} skill=' React ' />
+            <SkillList src={checkMarkIcon} skill=' Angular ' />
+            <SkillList src={checkMarkIcon} skill=' Vue ' />
+            <SkillList src={checkMarkIcon} skill=' Vite ' />
+            <SkillList src={checkMarkIcon} skill=' Python ' />
+        </div>
+        <hr />
+    </section>
+  )
+}
+
+export default Skills
